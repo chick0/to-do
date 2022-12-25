ac9fd6f6f2ca4a042796b7f4416b83485743011c
chick_0 <chick_0@ch1ck.xyz>
2022-12-26 03:30:37 +0900

    chore: Create login/sign-up form component

---
+++ src/lib/form/User.svelte
@@ -0,0 +1,63 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+    import { is_loading } from "src/store";
+
+    export let submit_text;
+
+    let email = "";
+    let password = "";
+
+    /** @type {HTMLInputElement} */
+    let password_field;
+
+    /** @type {HTMLButtonElement} */
+    let submit;
+
+    const dispatch = createEventDispatcher();
+
+    function action() {
+        dispatch("action", {
+            email: email,
+            password: password,
+        });
+    }
+</script>
+
+{#if $is_loading}
+    <div class="spinner"></div>
+{:else}
+    <div class="field">
+        <label for="email">이메일</label>
+        <input
+            type="email"
+            id="email"
+            placeholder="Email"
+            required
+            bind:value="{email}"
+            on:keydown="{(e) => {
+                if (e.key == 'Enter') {
+                    password_field.focus();
+                }
+            }}" />
+    </div>
+
+    <div class="field">
+        <label for="password">비밀번호</label>
+        <input
+            type="password"
+            id="password"
+            placeholder="Password"
+            required
+            minlength="8"
+            bind:this="{password_field}"
+            bind:value="{password}"
+            on:keydown="{(e) => {
+                if (e.key == 'Enter') {
+                    password_field.blur();
+                    submit.click();
+                }
+            }}" />
+    </div>
+
+    <button class="button max" type="submit" bind:this="{submit}" on:click="{() => action()}">{submit_text}</button>
+{/if}
--- src/routes/SignUp.svelte
+++ src/routes/SignUp.svelte
@@ -1,92 +1,54 @@
 <script>
     import { push } from "svelte-spa-router";
+    import User from "src/lib/form/User.svelte";
+    import { is_loading } from "src/store";
     import { SIGN_UP } from "src/url.js";
     import { is_login } from "src/user.js";
 
-    let is_loading = true;
+    function sign_up(event) {
+        $is_loading = true;
 
-    let email = "";
-    let password = "";
-    let password_input = undefined;
-    let submit = undefined;
+        fetch(SIGN_UP, {
+            method: "POST",
+            body: JSON.stringify({
+                email: event.detail.email,
+                password: event.detail.password,
+            }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+            .then((resp) => resp.json())
+            .then((json) => {
+                alert(json.message);
+
+                if (json.status) {
+                    step = 2;
+                    token = json.payload.token;
+                } else {
+                    $is_loading = false;
+                }
+            })
+            .catch(() => {
+                alert("알 수 없는 오류가 발생했습니다.");
+                $is_loading = false;
+            });
+    }
+
+    let step = 1;
+    let token = "";
 
     if (is_login()) {
         push("/todo");
     } else {
-        is_loading = false;
+        $is_loading = false;
     }
 </script>
 
 <div class="container">
     <h1>회원가입</h1>
 
-    {#if is_loading}
-        <div class="spinner"></div>
-    {:else}
-        <div class="field">
-            <label for="email">이메일</label>
-            <input
-                type="email"
-                id="email"
-                bind:value="{email}"
-                placeholder="Email"
-                required
-                on:keydown="{(e) => {
-                    if (e.key == 'Enter') {
-                        password_input.focus();
-                    }
-                }}" />
-        </div>
-
-        <div class="field">
-            <label for="password">비밀번호</label>
-            <input
-                type="password"
-                id="password"
-                placeholder="Password"
-                required
-                minlength="8"
-                bind:this="{password_input}"
-                bind:value="{password}"
-                on:keydown="{(e) => {
-                    if (e.key == 'Enter') {
-                        password_input.blur();
-                        submit.click();
-                    }
-                }}" />
-        </div>
-
-        <button
-            class="button max"
-            type="submit"
-            bind:this="{submit}"
-            on:click="{() => {
-                is_loading = true;
-
-                fetch(SIGN_UP, {
-                    method: 'POST',
-                    body: JSON.stringify({
-                        email: email,
-                        password: password,
-                    }),
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                })
-                    .then((resp) => resp.json())
-                    .then((json) => {
-                        alert(json.message);
-
-                        if (json.status) {
-                            push('/login');
-                        } else {
-                            is_loading = false;
-                        }
-                    })
-                    .catch(() => {
-                        alert('알 수 없는 오류가 발생했습니다.');
-                        is_loading = false;
-                    });
-            }}">회원가입</button>
+    {#if step == 1}
+        <User on:action="{sign_up}" submit_text="회원가입" />
     {/if}
 </div>
